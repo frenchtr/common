f1fac53e593005cb16c530df68b5cca0b4450073
Travis R. French <travis.french.1991@gmail.com>
2022-11-25 21:43:04 -0800

    [FEATURE] Added Registrar class. (#1)
    Added a Registrar class that entities can register / deregister from and be tracked while registered

---
+++ Runtime/Registration/Registrar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TravisRFrench.Common.Runtime.Registration
+{
+    public class Registrar<TEntity> : IRegistrar<TEntity>
+    {
+        protected List<TEntity> Entities { get; }
+        IEnumerable<TEntity> IRegistrar<TEntity>.Entities => this.Entities;
+
+        public Registrar()
+        {
+            this.Entities = new List<TEntity>();
+        }
+        
+        public void Register(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            if (this.Entities.Contains(entity))
+            {
+                return;
+            }
+            
+            this.Entities.Add(entity);
+        }
+
+        public void Deregister(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            
+            if (!this.Entities.Contains(entity))
+            {
+                return;
+            }
+            
+            this.Entities.Remove(entity);
+        }
+    }
+}
---
+++ Tests/Editor/EditorTestCategories.cs
@@ -0,0 +1,7 @@
+﻿namespace TravisRFrench.Common.Tests.Editor
+{
+    public static class EditorTestCategories
+    {
+        public const string UnitTests = nameof(EditorTestCategories.UnitTests);
+    }
+}
