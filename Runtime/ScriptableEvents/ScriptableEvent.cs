b7ed0d717f65d1a3ca49137ad3ef43a243df604c
Travis French <travis.french.1991@gmail.com>
2023-09-06 22:44:58 -0700

    Implemented ScriptableEvents.

---
+++ Runtime/ScriptableEvents/ScriptableEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TravisRFrench.Common.Runtime.ScriptableEvents
+{
+    [CreateAssetMenu(menuName = "Scriptables/Events/Scriptable Event")]
+    public class ScriptableEvent : ScriptableObject
+    {
+        [SerializeField]
+        private UnityEvent raised;
+
+        public event Action Raised;
+
+        public void Raise()
+        {
+            this.raised?.Invoke();
+            this.Raised?.Invoke();
+        }
+    }
+
+    public abstract class ScriptableEvent<TData> : ScriptableObject
+    {
+        [SerializeField]
+        private UnityEvent<TData> raised;
+
+        public event Action<TData> Raised;
+
+        public void Raise(TData data)
+        {
+            this.raised?.Invoke(data);
+            this.Raised?.Invoke(data);
+        }
+    }
+}
---
+++ Runtime/ScriptableEvents/ScriptableEventListener.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace TravisRFrench.Common.Runtime.ScriptableEvents
+{
+    public class ScriptableEventListener : MonoBehaviour
+    {
+        [SerializeField]
+        private ScriptableEvent @event;
+        [SerializeField]
+        private UnityEvent response;
+
+        protected virtual void Awake()
+        {
+            this.@event.Raised += this.OnEventRaised;
+        }
+        
+        protected virtual void OnDestroy()
+        {
+            this.@event.Raised -= this.OnEventRaised;
+        }
+
+        private void OnEventRaised()
+        {
+            this.response?.Invoke();
+        }
+    }
+
+    public abstract class ScriptableEventListener<TData> : MonoBehaviour
+    {
+        [SerializeField]
+        private ScriptableEvent<TData> @event;
+        [SerializeField]
+        private UnityEvent<TData> response;
+
+        protected virtual void Awake()
+        {
+            this.@event.Raised += this.OnEventRaised;
+        }
+        
+        protected virtual void OnDestroy()
+        {
+            this.@event.Raised -= this.OnEventRaised;
+        }
+
+        private void OnEventRaised(TData data)
+        {
+            this.response?.Invoke(data);
+        }
+    }
+}
